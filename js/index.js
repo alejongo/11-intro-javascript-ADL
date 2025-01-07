170fc0c9dfd9b953951664b0df45578705c615ba
alejongo <= alejongomezmontoya@gmail.com>
2025-01-07 18:30:39 -0500

    :zap: (Add quantity): Add quantity logic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-precio = 400000
+const price = document.querySelector('.precio-inicial')
+const quantity = document.querySelector('.cantidad')
+const add = document.querySelector('#add')
+const minus = document.querySelector('#minus')
+const total = document.querySelector('p .valor-total')
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+// Price definition
+const basePrice = 400000
+price.innerHTML = basePrice
+
+// Quantity definition
+const sellQuantity = 0
+quantity.innerHTML = sellQuantity
+
+// Total
+const totalCharge = quantity.innerHTML * price.innerHTML
+total.innerHTML = totalCharge
+
+
