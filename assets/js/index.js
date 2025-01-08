f9b4b60dae167c1d6d4d2d4464f6c89face183d7
alejongo <= alejongomezmontoya@gmail.com>
2025-01-08 08:30:26 -0500

    :zap: (Fix logic + -): Replace onlclick event for addEventListener function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,28 @@
 const price = document.querySelector('.precio-inicial')
 const quantity = document.querySelector('.cantidad')
 let total = document.querySelector('.valor-total')
+const add = document.querySelector('#add')
+const minus = document.querySelector('#minus')
 
-// Price definition
+
+// Price empty state
 const basePrice = 400000
 price.innerHTML = basePrice
 
-// Quantity definition
+// Quantity empty state
 let sellQuantity = 0
 quantity.innerHTML = sellQuantity
 
+// Increment and Decrement logic
+add.addEventListener('click', () => {
+    let cant = quantity.innerHTML = Number(quantity.innerHTML) + 1
+    let total = document.querySelector('.valor-total')
+    total.innerHTML = cant * basePrice
+})
 
-
+minus.addEventListener('click', () => {
+    cant = quantity.innerHTML = Number(quantity.innerHTML) - 1
+    total = document.querySelector('.valor-total')
+    total.innerHTML = cant * basePrice
+})
 
