a2b24db0398196fe2e186c4962a000cd1daac45b
alejongo <= alejongomezmontoya@gmail.com>
2025-01-07 19:31:35 -0500

    :zap: (Total charge): Total charge logic at button increment

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 const price = document.querySelector('.precio-inicial')
 const quantity = document.querySelector('.cantidad')
-const add = document.querySelector('#add')
-const minus = document.querySelector('#minus')
-const total = document.querySelector('p .valor-total')
+let total = document.querySelector('.valor-total')
 
 // Price definition
 const basePrice = 400000
 price.innerHTML = basePrice
 
 // Quantity definition
-const sellQuantity = 0
+let sellQuantity = 0
 quantity.innerHTML = sellQuantity
 
-// Total
-const totalCharge = quantity.innerHTML * price.innerHTML
-total.innerHTML = totalCharge
+
 
 
